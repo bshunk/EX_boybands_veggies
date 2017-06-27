532925a8a56b074874bccf63f77b83c82fdb8c69
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-06-27 12:07:10 -0500

    work on boybands JS exercise

--- boybands.js
+++ boybands.js
@@ -1,4 +1,4 @@
-// Requirements: Loop through the two arrays provided (bands and vegetables) and output each element in the arrays into their corresponding HTML element. Ensure that each item is in a block element (e.g. li, div, p. etc...)
+console.log("Hello there, mate!")
 
 var bands = ["Boyz II Men", "NSync", "New Kids on the Block", "98 Degrees", "One Direction"];
 var vegetables = ["Carrots", "Kale", "Zucchini", "Broccoli", "Squash"];
@@ -13,19 +13,28 @@ var currentBand = "";
 var currentVeggie = "";
 
 // Get a reference to the appropriate DOM element for bands
-var bandElement = document.getElementById(???);
+var bandElement = document.getElementById("boy-bands");
 
 // Get a reference to the appropriate DOM element for vegetables
-var veggieElement = ???;
+var veggieElement = document.getElementById("vegetables");
 
 // Start looping
 for (var loopTracker = 0; loopTracker < loopCount; loopTracker += 1) {
   
-  // Add the band names into the correct <div>
-  currentBand = ???;
+// Add the band names into the correct <div>
+	currentBand = bands[loopTracker];
+  console.log("current band is", currentBand);
+  bandElement.innerHTML += `<div class="blue">${currentBand} </div>`;
 
-  
-  // Add the veggie names into the correct <div>
-  currentVeggie = ???;
+// Add the veggie names into the correct <div>
+	currentVeggie = vegetables[loopTracker];
+	console.log("current veggie is", currentVeggie);
+	veggieElement.innerHTML += `<div class="green">${currentVeggie}</div>`;
+};
 
-}
+
+
+// Requirements: Loop through the two arrays provided (bands and vegetables) and output each element in the arrays into their corresponding HTML element. Ensure that each item is in a block element (e.g. li, div, p. etc...)
+
+
+
