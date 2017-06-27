2466170540d5d66516d04f58bfd9495ed1695a95
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-06-27 11:32:12 -0500

    start anew on boybands

--- boybands.js
+++ boybands.js
@@ -1,3 +1,5 @@
+// Requirements: Loop through the two arrays provided (bands and vegetables) and output each element in the arrays into their corresponding HTML element. Ensure that each item is in a block element (e.g. li, div, p. etc...)
+
 var bands = ["Boyz II Men", "NSync", "New Kids on the Block", "98 Degrees", "One Direction"];
 var vegetables = ["Carrots", "Kale", "Zucchini", "Broccoli", "Squash"];
 
@@ -7,44 +9,23 @@ var loopCount = 5;
 // Keep track of which band we're on in the loop
 var currentBand = "";
 
-var bandList = "";
-
 // Keep track of which veggie we're on in the loop
 var currentVeggie = "";
 
-var veggieList = "";
-
 // Get a reference to the appropriate DOM element for bands
-var bandElement = document.getElementById("boy-bands");
+var bandElement = document.getElementById(???);
 
 // Get a reference to the appropriate DOM element for vegetables
-var veggieElement = document.getElementById("vegetables");
+var veggieElement = ???;
 
 // Start looping
 for (var loopTracker = 0; loopTracker < loopCount; loopTracker += 1) {
   
-	// Add the band names into the correct <div>
-	currentBand = bands[loopTracker];
+  // Add the band names into the correct <div>
+  currentBand = ???;
 
   
-	// Add the veggie names into the correct <div>
-	currentVeggie = vegetables[loopTracker];
+  // Add the veggie names into the correct <div>
+  currentVeggie = ???;
 
-	// bandList = bandList + "<div id='boy-bands'>" + currentBand;
-	// bandList = bandList  + "</div>";
-
-	// veggieList = veggieList + "<div id='vegetables'>" + currentVeggie;
-	// veggieList = veggieList + "</div>";
-
-};
-
-// console.log("currentBand", currentBand);
-// console.log("currentVeggie", currentVeggie);
-
-// bandElement.innerHTML = bandList;
-// veggieElement.innerHTML = veggieList;
-
-
-
-
-
+}
